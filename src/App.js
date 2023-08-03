37add09f37146c894e191ad4692248ac0110060a
HanneSolo <Haasrx@gmail.com>
2023-08-03 12:54:11 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=8f33a809';
+
+
+
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+       
+    useEffect (() => {
+        searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Movie Land</h1>
+
+            <div className="search">
+                <input 
+                    value={searchTerm}
+                    type='text' 
+                    placeholder='Search for a movie'
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    
+                />
+
+                <img
+                    src={SearchIcon}
+                    alt="search icon"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+
+            </div>
+
+            {
+                movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie = {movie} />
+                        ))}
+                    </div>
+                ) :
+                (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+
+            
+        </div>
+    );
+}
+
+export default App;
